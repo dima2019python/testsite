7d8c0a33c9169b687e5d7eb9e34a29ecf685adcb
dima2019python <d.shkatula2019@gmail.com>
2021-04-20 16:12:46 +0400

    is_published = models.BooleanField(default=True)

--- my_site/my_site/urls.py
+++ my_site/my_site/urls.py
@@ -20,7 +20,7 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('news/', include('news.urls')),
+    path('', include('news.urls')),
 ]
 
 if settings.DEBUG:
--- my_site/news/views.py
+++ my_site/news/views.py
@@ -1,9 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import News
 
 
 def index(request):
-    return HttpResponse('<h1>Hello World -**-</h1>')
-
-def test(request):
-    return HttpResponse('<h1>Тестовая страница<h1>')
+    news = News.objects.order_by('-created_at')
+    return render(request, 'news/index.html', {'news': news, 'title': 'Список новостей'})
